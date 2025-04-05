8f071b306268966d9ff9771b8ff1d5b9365c88f9
Vinícius Dias <vyvy292004@gmail.com>
2025-04-05 19:19:43 -0300

    updates class demostration

--- class_demo.py
+++ class_demo.py
@@ -8,10 +8,10 @@ import functions
 
 
 raizPrimeiraFuncao, interacoesPrimeiraFuncao = regula_falsi(functions.primeiraFuncao, 2, 3)
-exibir_chutes(interacoesPrimeiraFuncao, 'Método numérico: falsa posição')
+exibir_chutes(interacoesPrimeiraFuncao, 'Método numérico: falsa posição', interacoesPrimeiraFuncao[-1])
 raizSegundaFuncao, interacoesSegundaFuncao = ponto_fixo(functions.segundaFuncaoInteracao, 0.5)
-exibir_chutes(interacoesSegundaFuncao, 'Método numérico: ponto fixo')
+exibir_chutes(interacoesSegundaFuncao, 'Método numérico: ponto fixo', interacoesSegundaFuncao[-1])
 raizTerceiraFuncao, interacoesTerceiraFuncao = newton_raphson(functions.terceiraFuncao, functions.derivadaTerceiraFuncao, 1.5)
-exibir_chutes(interacoesTerceiraFuncao, 'Método numérico: newton-raphson')
+exibir_chutes(interacoesTerceiraFuncao, 'Método numérico: newton-raphson', interacoesTerceiraFuncao[-1])
 raizQuartaFuncao, interacoesQuartaFuncao = bissecao(functions.quartaFuncao, 0, 1)
-exibir_chutes(interacoesQuartaFuncao, 'Método numérico: bisseção')
+exibir_chutes(interacoesQuartaFuncao, 'Método numérico: bisseção', interacoesQuartaFuncao[-1])
